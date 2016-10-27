bf59306bba38755b515e0fb4fc2aba9bf7ab0532
Alasdair Gray <A.Gray-17@sms.ed.ac.uk>
2016-10-28 00:06:13 +0100

    added RSM simulations from VLX

--- Assignment 1/Simulation Files/Batch Run/CFD5_A1_Batch_Run.sh
+++ Assignment 1/Simulation Files/Batch Run/CFD5_A1_Batch_Run.sh
@@ -1,29 +1,29 @@
-starccm+ -batch -np 8 KW_ND_coarse_grid.sim
+#starccm+ -batch -np 8 KW_ND_coarse_grid.sim
 
-starccm+ -batch -np 8 KW_WD_coarse_grid.sim
+#starccm+ -batch -np 8 KW_WD_coarse_grid.sim
 
-starccm+ -batch -np 8 KWSST_ND_coarse_grid.sim
+#starccm+ -batch -np 8 KWSST_ND_coarse_grid.sim
 
-starccm+ -batch -np 8 KWSST_WD_coarse_grid.sim
+#starccm+ -batch -np 8 KWSST_WD_coarse_grid.sim
 
-starccm+ -batch -np 8 KW_ND_med_grid.sim
+#starccm+ -batch -np 8 KW_ND_med_grid.sim
 
-starccm+ -batch -np 8 KW_WD_med_grid.sim
+#starccm+ -batch -np 8 KW_WD_med_grid.sim
 
-starccm+ -batch -np 8 KWSST_ND_med_grid.sim
+#starccm+ -batch -np 8 KWSST_ND_med_grid.sim
 
-starccm+ -batch -np 8 KWSST_WD_med_grid.sim
+#starccm+ -batch -np 8 KWSST_WD_med_grid.sim
 
-starccm+ -batch -np 8 KW_ND_fine_grid.sim
+#starccm+ -batch -np 8 KW_ND_fine_grid.sim
 
-starccm+ -batch -np 8 KW_WD_fine_grid.sim
+#starccm+ -batch -np 8 KW_WD_fine_grid.sim
 
-starccm+ -batch -np 8 KWSST_ND_fine_grid.sim
+#starccm+ -batch -np 8 KWSST_ND_fine_grid.sim
 
-starccm+ -batch -np 8 KWSST_WD_fine_grid.sim
+#starccm+ -batch -np 8 KWSST_WD_fine_grid.sim
 
-starccm+ -batch -np 8 RSM_coarse_grid.sim
+starccm+ -batch RSM_coarse_grid.sim
 
-starccm+ -batch -np 8 RSM_med_grid.sim
+starccm+ -batch RSM_med_grid.sim
 
-starccm+ -batch -np 8 RSM_fine_grid.sim
+starccm+ -batch RSM_fine_grid.sim
